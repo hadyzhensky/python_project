aa4e2045959d9378b5f9d558b24ac25de1e6f92f
hadyzhensky <91096898+hadyzhensky@users.noreply.github.com>
2026-03-21 16:17:53 +0300

    add superuser and new class

--- trainer/models.py
+++ trainer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class SQLTask(models.Model):
+    title = models.CharField(max_length=200)         # название задачи
+    description = models.TextField()                 # описание задачи
+    correct_query = models.TextField()               # правильный SQL
+    created_at = models.DateTimeField(auto_now_add=True)  # дата создания
+
+    def __str__(self):
+        return self.title
